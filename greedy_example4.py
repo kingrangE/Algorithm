e4016f7814e329f625eb446f93f19f86525025af
kingrangE <yuvw123@gmail.com>
2024-07-21 19:07:16 +0900

    그리디 마지막 문제

---
+++ greedy_example4.py
@@ -0,0 +1,14 @@
+N,K = map(int,input().split())
+cnt = 0
+
+while True :
+    if N%K == 0 : #나누어 떨어지면
+        N //= K
+        cnt += 1
+    else : #나누어 떨어지지 않으면
+        N -= 1
+        cnt += 1
+    print(f"N = {N} cnt = {cnt}")
+    if(N == 1) : 
+        break
+print(cnt)
